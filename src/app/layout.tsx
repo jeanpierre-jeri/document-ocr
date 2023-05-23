c27a4bb3e050f1f1b2de8939c9cd83d9549c9073
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-23 01:10:35 -0500

    feat: add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,20 @@ const poppins = Poppins({
 
 export const metadata = {
   title: 'Document OCR',
-  description: 'Generate a word document from an image'
+  description: 'Generate a word document from an image',
+  openGraph: {
+    title: 'Document OCR',
+    description: 'Generate a word document from an image',
+    images: [{ url: 'https://document-ocr.vercel.app/og.jpg' }],
+    type: 'website',
+    url: 'https://document-ocr.vercel.app'
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Document OCR',
+    description: 'Generate a word document from an image',
+    images: [{ url: 'https://document-ocr.vercel.app/og.jpg' }]
+  }
 }
 
 export default function RootLayout ({
