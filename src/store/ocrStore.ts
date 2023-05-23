dd167825ab07ca08d3f94922fb8bd3675cbb0134
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-23 00:57:54 -0500

    feat: add language and filename

--- src/store/ocrStore.ts
+++ src/store/ocrStore.ts
@@ -4,16 +4,24 @@ interface OcrStore {
   status: string
   progress: number
   result: string
+  filename: string
+  language: 'eng' | 'spa' | ''
+  setLanguage: (language: 'eng' | 'spa' | '') => void
   setStatus: (status: string) => void
   setProgress: (progress: number) => void
   setResult: (result: string) => void
+  setFilename: (filename: string) => void
 }
 
 export const useOcrStore = create<OcrStore>()((set) => ({
   status: '',
   progress: 0,
   result: '',
-  setStatus: (status) => set(({ status })),
-  setProgress: (progress) => set(({ progress })),
-  setResult: (result) => set(({ result }))
+  filename: '',
+  language: '',
+  setLanguage: (language) => set({ language }),
+  setStatus: (status) => set({ status }),
+  setProgress: (progress) => set({ progress }),
+  setResult: (result) => set({ result }),
+  setFilename: (filename) => set({ filename })
 }))
