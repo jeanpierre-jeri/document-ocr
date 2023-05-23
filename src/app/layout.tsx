a03c92af1829e8a603857a58a7b345b6d9d68e82
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-23 00:57:17 -0500

    feat: add sonner toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from '@/components/Toaster'
 import './globals.css'
 import { Poppins } from 'next/font/google'
 
@@ -19,7 +20,9 @@ export default function RootLayout ({
 }) {
   return (
     <html lang='en'>
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>{children}
+        <Toaster />
+      </body>
     </html>
   )
 }
