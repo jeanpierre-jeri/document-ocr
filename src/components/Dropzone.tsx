11561ca684dd822e2fae3de9d56a5f1ba572199c
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-23 00:57:44 -0500

    feat: add language change

--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -1,26 +1,29 @@
-'use client'
-
 import { useCallback, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { ImagePlaceholder } from './ImagePlaceholder'
-import Tesseract from 'tesseract.js'
+import { createWorker } from 'tesseract.js'
 import { useOcrStore } from '@/store/ocrStore'
+import { toast } from 'sonner'
 
 export function Dropzone () {
   const [image, setImage] = useState('')
 
-  const [setStatus, setProgress, setResult] = useOcrStore(state => [state.setStatus, state.setProgress, state.setResult])
+  const [setStatus, setProgress, setResult, setFilename, setLanguage] = useOcrStore(state => [state.setStatus, state.setProgress, state.setResult, state.setFilename, state.setLanguage])
+  const language = useOcrStore(state => state.language)
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     const url = URL.createObjectURL(acceptedFiles[0])
+    const filename = acceptedFiles[0].name.replace(/\.\w+/gm, '')
     setImage(url)
-  }, [])
+    setFilename(filename)
+  }, [setFilename])
 
   const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
     onDrop,
     accept: {
       'image/jpeg': ['.jpeg', '.jpg'],
-      'image/png': ['.png']
+      'image/png': ['.png'],
+      'image/webp': ['.webp']
     },
     maxFiles: 1
   })
@@ -30,25 +33,41 @@ export function Dropzone () {
       open()
       return
     }
-    const data = await Tesseract.recognize(
-      image,
-      'eng',
-      {
-        logger: m => {
-          setStatus(m.status)
-          setProgress(m.progress)
-        }
+
+    if (language === '') {
+      toast.error('Please select a language')
+      return
+    }
+
+    const worker = await createWorker({
+      logger: m => {
+        setStatus('Loading...')
+        setProgress(m.progress)
       }
-    )
+    })
+
+    await Promise.all([
+      await worker.loadLanguage(language),
+      await worker.initialize(language)
+    ])
+
+    const data = await worker.recognize(image)
 
     setResult(data.data.text)
+    toast.success('Done!')
+    await worker.terminate()
+  }
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const lang = e.currentTarget.value as 'eng' | 'spa'
+    setLanguage(lang)
   }
 
   return (
     <>
       <div
         className={`
-          w-full h-56 rounded-xl bg-[#1f2123]  mt-8 flex items-center justify-center  overflow-hidden
+          w-full aspect-square md:aspect-video 2xl:aspect-square rounded-xl bg-[#1f2123] mt-8 flex items-center justify-center  overflow-hidden
           ${image === '' ? 'p-9 border-dashed border border-[#637ca0]' : ''}
         `}
         {...getRootProps()}
@@ -59,9 +78,18 @@ export function Dropzone () {
 
       <p className='text-xs text-[#BDBDBD] tracking-tight text-center mt-4'>Or</p>
 
-      <button onClick={handleClick} type='button' className='bg-[#2F80ED] hover:bg-[#146DE1] transition-colors rounded-lg px-4 py-3 text-white text-xs tracking-tight mx-auto block text-center mt-5'>
-        {image === '' ? 'Choose a file' : 'Upload'}
-      </button>
+      <div className='flex items-center flex-col mt-5'>
+        <label htmlFor='language' className='block text-sm font-medium text-gray-900 dark:text-white'>Select a language</label>
+        <select onChange={handleSelectChange} id='counlanguagetries' className='bg-[#1f2123] text-white text-sm rounded-lg block p-2.5 w-fit mt-2 mb-5' defaultValue=''>
+          <option value='' disabled>Choose a language</option>
+          <option value='eng'>English</option>
+          <option value='spa'>Spanish</option>
+        </select>
+
+        <button onClick={handleClick} type='button' className='bg-[#2F80ED] hover:bg-[#146DE1] transition-colors rounded-lg px-4 py-3 text-white text-xs tracking-tight mx-auto block text-center '>
+          {image === '' ? 'Choose a file' : 'Start'}
+        </button>
+      </div>
 
     </>
   )
