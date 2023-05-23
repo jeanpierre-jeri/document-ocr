bb00705433c0e26f5da11ea0eac129eadafd9fa5
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-23 01:55:09 -0500

    feat: add copy to clipboard button

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,6 +1,7 @@
 import { useOcrStore } from '@/store/ocrStore'
 import { useMemo, useState } from 'react'
-import { DoneIcon, RestartIcon } from './Icons'
+import { CopyIcon, DoneIcon, RestartIcon } from './Icons'
+import { toast } from 'sonner'
 
 export function Result () {
   const result = useOcrStore(state => state.result)
@@ -17,7 +18,7 @@ export function Result () {
 
   if (result === '') return null
 
-  const handleClick = () => {
+  const handleRestart = () => {
     setStatus('')
     setProgress(0)
     setResult('')
@@ -26,13 +27,25 @@ export function Result () {
     setName('')
   }
 
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(result)
+    toast.success('Copied to clipboard!')
+  }
+
   return (
     <div>
       <div className='w-9 aspect-square rounded-full bg-[#219653] flex justify-center items-center relative mx-auto mb-6'>
         <DoneIcon className='text-white absolute w-6' />
       </div>
 
-      <textarea className='text-white text-base w-full aspect-[1/1.2] bg-[#1f1f20] resize-none scrollbar-track-[#3f3f3f] scrollbar-thumb-[#2f80ed] scrollbar-thin scrollbar-thumb-rounded-lg p-2' defaultValue={result} />
+      <div className='relative'>
+        <textarea className='text-white text-base w-full aspect-[1/1.2] bg-[#1f1f20] resize-none scrollbar-track-[#3f3f3f] scrollbar-thumb-[#2f80ed] scrollbar-thin scrollbar-thumb-rounded-lg p-2' defaultValue={result} />
+        <button onClick={handleCopy} type='button' className='absolute bottom-4 right-4 bg-black/70 rounded-lg p-2'>
+          <i className='relative flex items-center justify-center text-white w-6 aspect-square'>
+            <CopyIcon className='absolute w-full' />
+          </i>
+        </button>
+      </div>
 
       <div className='grid grid-cols-12 mt-5 rounded-lg overflow-hidden w-full'>
         <input type='text' placeholder='Your document name' className='block col-span-9 h-fit items-center px-4 py-3 outline-none w-full bg-[#3b3b3b] text-white' value={name} onInput={(e) => setName(e.currentTarget.value)} />
@@ -41,10 +54,10 @@ export function Result () {
         </a>
       </div>
 
-      <button onClick={handleClick} type='button' className='text-black bg-yellow-400 hover:bg-yellow-500 transition-colors mx-auto mt-4 flex items-center rounded-lg px-4 py-3 gap-2 h-fit'>
+      <button onClick={handleRestart} type='button' className='text-black bg-yellow-400 hover:bg-yellow-500 transition-colors mx-auto mt-4 flex items-center rounded-lg px-4 py-3 gap-2 h-fit'>
         Restart
         <i className='w-6 aspect-square relative flex items-center justify-center'>
-          <RestartIcon className='w-6 absolute' />
+          <RestartIcon className='w-full absolute' />
         </i>
       </button>
 
