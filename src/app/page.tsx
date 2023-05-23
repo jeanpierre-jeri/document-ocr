84b1b69a82622f8881178502c762c7d549092b73
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-23 00:57:33 -0500

    styles: increase max width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Main } from '@/components/Main'
 export default async function Home () {
   return (
     <main className='bg-[#1f1f20] flex items-center justify-center min-h-screen font-medium'>
-      <section className='bg-[#121212] py-9 px-8 rounded-xl shadow-md shadow-white/10 w-full max-w-md'>
+      <section className='bg-[#121212] py-9 px-8 rounded-xl shadow-md shadow-white/10 w-full max-w-lg m-4'>
         <Main />
       </section>
 
